e87e66af8ef1b3b5813be1822c89063dc860947a
Anders Lövgren <andlov@nowise.se>
2019-10-03 05:34:17 +0200

    More sender class examples.

--- example/sender.cpp
+++ example/sender.cpp
@@ -13,18 +13,32 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
 #include "logging.hpp"
 #include <iostream>
 
+using namespace Citrus::Logging;
+
 int main()
 {
-        using Sender = Citrus::Logging::Sender;
-
         Sender sender;
+
+        TargetFile target1("/tmp/output1.txt");
+        TargetFile target2("/tmp/output2.txt");
+        
+        TargetStream target3(std::cout, RecordFormat<FormatJson>::Object());
+        TargetStream target4(std::cout, RecordFormat<FormatText>::Object());
+        TargetStream target5(std::cerr);
+
+        sender.Register(&target1, Level::Debug, Level::Warning);
+        sender.Register(&target2, Level::Error, Level::Emergent);
+        sender.Register(&target3, Level::Debug, Level::Warning);
+        sender.Register(&target4, Level::Debug, Level::Warning);
+        sender.Register(&target5, Level::Error, Level::Emergent);
+
+        sender.Write(Record());
+        sender.Write(Level::Debug, "citrus-logger", "Hello World!");
+        sender.Write(Level::Error, "citrus-logger", "Hello World!");
+        sender.Write(Level::Error, "Hello World!");
 
         return 0;
 }
